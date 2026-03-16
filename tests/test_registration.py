05d1f55a3957c4558f590e060a48aa68d2377cbd
Neo <skytalker89@gmail.com>
2026-03-16 23:06:27 +0300

    add first kafka producer teset

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,5 +1,7 @@
+import json
 import time
 import uuid
+from kafka import KafkaProducer
 
 from framework.internal.http.account import AccountApi
 from framework.internal.http.mail import MailApi
@@ -15,10 +17,39 @@ def test_failed_registration(account: AccountApi, email: MailApi) -> None:
             raise AssertionError("Email not found")
         time.sleep(1)
 
+
 def test_successful_registration(account: AccountApi, email: MailApi) -> None:
-    base =uuid.uuid4().hex
+    base = uuid.uuid4().hex
     account.register_user(login=base, password="123123", email=f"{base}@email.ru")
     email.find_message(query="string")
+    for _ in range(10):
+        response = email.find_message(query=base)
+        if response.json()["total"] > 0:
+            break
+        time.sleep(1)
+    else:
+        raise AssertionError("Email not found")
+
+
+def test_successful_registration_with_kafka_producer(account: AccountApi, email: MailApi) -> None:
+    base = uuid.uuid4().hex
+    message = {
+        "login": base,
+        "password": "123123",
+        "email": f"{base}@email.ru",
+    }
+    producer = KafkaProducer(bootstrap_servers=['185.185.143.231:9092'],
+                             value_serializer=lambda m: json.dumps(m).encode("utf-8"),
+                             acks="all",
+                             retries=5,
+                             retry_backoff_ms=5000,
+                             request_timeout_ms=70000,
+                             connections_max_idle_ms=65000,
+                             reconnect_backoff_ms=10000,
+                             reconnect_backoff_max_ms=10000,
+                             )
+    producer.send('register-events', message)
+    producer.close()
     for _ in range(10):
         response = email.find_message(query=base)
         if response.json()["total"] > 0:
