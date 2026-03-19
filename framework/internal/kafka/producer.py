9e8176d74451d27be63d7324ee9be8f8dd56b1f2
Neo <skytalker89@gmail.com>
2026-03-19 20:54:57 +0300

    refactor kafka producer

---
+++ framework/internal/kafka/producer.py
@@ -0,0 +1,50 @@
+import json
+import threading
+from tracemalloc import Traceback
+from types import TracebackType
+from typing import Any
+
+from kafka import KafkaProducer
+from kafka.benchmarks.load_example import Producer
+
+
+class Producer:
+    def __init__(self, bootstrap_servers: list[str] = ['185.185.143.231:9092']):
+        self._bootstrap_servers = bootstrap_servers
+        self._producer: KafkaProducer | None = None
+        self._lock: threading.Lock = threading.Lock()
+
+    def start(self) -> None:
+        self._producer = KafkaProducer(bootstrap_servers=self._bootstrap_servers,
+                                       value_serializer=lambda m: json.dumps(m).encode("utf-8"),
+                                       acks="all",
+                                       retries=5,
+                                       retry_backoff_ms=5000,
+                                       request_timeout_ms=70000,
+                                       connections_max_idle_ms=65000,
+                                       reconnect_backoff_ms=10000,
+                                       reconnect_backoff_max_ms=10000,
+                                       )
+
+    def stop(self) -> None:
+        if self._producer:
+            self._producer.close()
+            self._producer = None
+
+    def send(self, topic: str, message: dict[str, str]) -> None:
+        if not self._producer:
+            raise RuntimeError('Producer must be started before sending message')
+        try:
+            with self._lock:
+                future = self._producer.send(topic, value=message)
+                record_metadata = future.get(timeout=10)
+                return record_metadata
+        except Exception as e:
+            raise RuntimeError('Failed to send message: {}'.format(e))
+
+    def __enter__(self) -> "Producer":
+        self.start()
+        return self
+
+    def __exit__(self, exc_type: type[BaseException], exc_val: BaseException | None, exc_tb: TracebackType) -> None:
+        self.stop()
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -5,6 +5,7 @@ from kafka import KafkaProducer
 
 from framework.internal.http.account import AccountApi
 from framework.internal.http.mail import MailApi
+from framework.internal.kafka.producer import Producer
 
 
 def test_failed_registration(account: AccountApi, email: MailApi) -> None:
@@ -31,25 +32,15 @@ def test_successful_registration(account: AccountApi, email: MailApi) -> None:
         raise AssertionError("Email not found")
 
 
-def test_successful_registration_with_kafka_producer(account: AccountApi, email: MailApi) -> None:
+def test_successful_registration_with_kafka_producer(email: MailApi, kafka_producer: Producer) -> None:
     base = uuid.uuid4().hex
     message = {
         "login": base,
         "password": "123123",
         "email": f"{base}@email.ru",
     }
-    producer = KafkaProducer(bootstrap_servers=['185.185.143.231:9092'],
-                             value_serializer=lambda m: json.dumps(m).encode("utf-8"),
-                             acks="all",
-                             retries=5,
-                             retry_backoff_ms=5000,
-                             request_timeout_ms=70000,
-                             connections_max_idle_ms=65000,
-                             reconnect_backoff_ms=10000,
-                             reconnect_backoff_max_ms=10000,
-                             )
-    producer.send('register-events', message)
-    producer.close()
+
+    kafka_producer.send('register-events', message)
     for _ in range(10):
         response = email.find_message(query=base)
         if response.json()["total"] > 0:
