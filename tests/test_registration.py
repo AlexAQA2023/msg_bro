cd23230e91261452ae135fad2572d0350a22d290
Neo <skytalker89@gmail.com>
2026-03-24 23:23:27 +0300

    add first consumer try

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,8 +1,10 @@
+import json
 import time
 import uuid
 from framework.internal.http.account import AccountApi
 from framework.internal.http.mail import MailApi
 from framework.internal.kafka.producer import Producer
+from kafka import KafkaConsumer
 
 
 def test_failed_registration(account: AccountApi, email: MailApi) -> None:
@@ -45,3 +47,23 @@ def test_successful_registration_with_kafka_producer(email: MailApi, kafka_produ
         time.sleep(1)
     else:
         raise AssertionError("Email not found")
+
+def test_successful_registration_with_kafka_producer_consumer(kafka_producer: Producer) -> None:
+    base = uuid.uuid4().hex
+    message = {
+        "login": base,
+        "password": "123123",
+        "email": f"{base}@email.ru",
+    }
+
+    kafka_producer.send('register-events', message)
+    consumer = KafkaConsumer(
+        "register-events",
+        bootstrap_servers=['185.185.143.231:9092'],
+        auto_offset_reset='earliest',
+        value_deserializer=lambda x: json.loads(x.decode('utf-8')),
+    )
+    for message in consumer:
+        if message.value["login"] == base:
+            break
+    consumer.close()
