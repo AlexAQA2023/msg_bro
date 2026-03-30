cfdfba9b3978404da65697bec046e8efc00ffeea
Neo <skytalker89@gmail.com>
2026-03-30 18:00:52 +0300

    add thread

--- consumer.py
+++ consumer.py
@@ -1,20 +1,22 @@
 import json
 import threading
 import time
-
+import queue
 from kafka import KafkaConsumer
 
+q = queue.Queue()
 
 def consume_messages():
     consumer = KafkaConsumer(
         "register-events",
         bootstrap_servers=['185.185.143.231:9092'],
-        auto_offset_reset='earliest',
+        auto_offset_reset='latest',
         value_deserializer=lambda x: json.loads(x.decode('utf-8')),
     )
     try:
         for message in consumer:
             print(message.value["login"])
+            q.put(message)
     except Exception as e:
         print(e)
     finally:
@@ -22,5 +24,12 @@ def consume_messages():
 
 thread = threading.Thread(target=consume_messages, daemon=True)
 thread.start()
-time.sleep(10)
+time.sleep(1)
+
+def get_message():
+    try:
+        return q.get(timeout=90)
+    except queue.Queue:
+        raise AssertionError("Queue is empty")
+print(get_message())
 print('Stop')
